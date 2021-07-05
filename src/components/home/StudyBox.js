c4e9d9c0c9aba71f222b5a0ba2c2d0dfaf3f2182
Sue <skjung247@gmail.com>
2021-07-06 04:10:19 +0900

    Feature: seperate each tag & add css

--- src/components/home/StudyBox.js
+++ src/components/home/StudyBox.js
@@ -1,46 +1,63 @@
-import React from 'react';
+import React, {Component} from 'react';
 // import ".../assets/css/studyBox.css"
 
-function StudyBox({study}){
-    const { title, keyword, parties, period} = study || {};
-    const boxstyle = {
-        border: '1px solid gray',
-        borderRadius: '1em',
-        color: 'blue',
-        display: 'flex',
-        width: '30%',
-        padding: '5px',
-        margin: '5px'
-    }
-    const tagstyle = {
-        margin: '0px 2px',
-        color: 'rgb(255, 255, 255)',
-        padding: '3px 6px',
-        fontSize: '0.8em',
-        borderRadius: '0.3rem',
-        backgroundColor: 'rgb(0, 181, 173)',
-    
-    }
-    return (
-        <div>
-            <div className="studyBox" style={boxstyle}>
-                <div className="studyContents">
-                    <table className="studyBox-table">
-                        <tr>
-                            <td><strong>스터디제목{title}</strong></td>
-                        </tr>
-                        <tr>
-                            <td><text className="tag" style={tagstyle}>태그{keyword}</text></td>
-                        </tr>
-                        <tr>
-                            <td>인원{parties}</td>
-                            <td>기간{period}</td>
-                        </tr>
-                    </table>
+
+// const tags = ['NCS', '컴퓨터', 'TOEIC'];
+const boxstyle = {
+    border: '1px solid gray',
+    borderRadius: '1em',
+    color: 'blue',
+    display: 'flex',
+    justifyContent: 'center',
+    width: '30%',
+    padding: '5px',
+    margin: '5px'
+}
+const tagTd = {
+    display: 'flex',
+    justifyContent: 'space-evenly'
+}
+const tagstyle = {
+    margin: '0px 2px',
+    color: 'rgb(255, 255, 255)',
+    padding: '3px 6px',
+    fontSize: '0.8em',
+    borderRadius: '0.3rem',
+    backgroundColor: 'rgb(0, 181, 173)',
+
+}
+
+
+
+class StudyBox extends Component {        
+    render(){
+        const keywords = ["변수", "리스트", "네"];
+        const keyword = []
+        for (const [index, value] of keywords.entries()){
+            keyword.push(<text className="tag" style={tagstyle} key={index}>{value}</text>)
+        }
+        return(
+            <div>
+                <div className="studyBox" style={boxstyle}>
+                    <div className="studyContents">
+                        <table className="studyBox-table">
+                            <tr>
+                                <td><strong>스터디제목</strong></td>
+                            </tr>
+                            <tr>
+                                <td className="tag-td" style={tagTd}>{keyword}</td>
+                            </tr>
+                            <tr>
+                                <tr>인원</tr>
+                                <tr>기간</tr>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    );    
+        );
+    }
+ 
 }
 
 export default StudyBox;
