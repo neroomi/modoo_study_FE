c983fb26ba1bb970358e26bc43c43ad9a4e28867
Sue <skjung247@gmail.com>
2021-07-06 23:56:07 +0900

    Refactor: studyList & Feature: category filter by interest

--- src/components/home/StudyBox.js
+++ src/components/home/StudyBox.js
@@ -3,32 +3,7 @@ import "../../assets/css/studyBox.css"
 
 
 
-const studies = [
-    {
-        title: "면접스터디",
-        tags: ["NCS", "IT", "영어"],
-        person: ["10", "20"],
-        period: ["2021-08-01", "2021-08-30"]
-    },
-    {
-        title: "코딩스터디",
-        tags: ["컴퓨터", "IT", "개발",],
-        person: ["7", "10"],
-        period: ["2021-07-01", "2021-07-30"],
-    },
-    {
-        title: "영어스터디",
-        tags: ["회화", "언어", "토익"],
-        person: ["5", "8"],
-        period: ["2021-06-01", "2021-06-30"]
-    },
-    {
-        title: "자격증스터디",
-        tags: ["IT", "기사", "자격증"],
-        person: ["8", "10"],
-        period: ["2021-05-01", "2021-05-30"]
-    },
-]
+
 
 function StudyTags({multiTags}){
     console.log(multiTags);
@@ -103,7 +78,7 @@ function StudyContents({title, tags, person, period}){
 }
 
 
-function StudyBox() { 
+function StudyBox({studies}) { 
     return(
         <div>
             {studies.map(studyInfo => (
